84ff77cbd428110a73d4d8c9476e222c89054738
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-28 01:57:39 +0300

    feat(auth): wire login to AuthService and redirect

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly ACCESS_TOKEN_KEY = 'access_token';
+  login(email: string , password: string):void {
+    localStorage.setItem(this.ACCESS_TOKEN_KEY, 'mock_token_value');
+  }
+  logout():void {
+    localStorage.removeItem(this.ACCESS_TOKEN_KEY);
+  }
+  getAccessToken():string | null {
+    return localStorage.getItem(this.ACCESS_TOKEN_KEY);
+  }
+}
