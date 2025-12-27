8299a49c6575e29b8d66ada5bca474af230fa036
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-28 01:25:56 +0300

    feat: improve login validation UX with submitted flag

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+submitted = false;
 
+fb = inject(FormBuilder);
+form = this.fb.group({
+  email : ['', [Validators.required, Validators.email]],
+  password : ['', [Validators.required, Validators.minLength(6)]]
 }
+)
+onSubmit() {
+this.submitted = true;
+if(this.form.invalid){
+  return;
+}
+console.log(this.form.value);
+this.form.reset();
+this.submitted = false;
+}}
