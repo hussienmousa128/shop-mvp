ebfc944d70b6f4b1771c1c138e7353d4f0395569
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-28 02:30:01 +0300

    refactor(auth): cleanup login and header

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +13,22 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class Login {
 submitted = false;
-
+auth = inject(AuthService);
+router = inject(Router)
 fb = inject(FormBuilder);
 form = this.fb.group({
   email : ['', [Validators.required, Validators.email]],
   password : ['', [Validators.required, Validators.minLength(6)]]
 }
 )
+email = this.form.value.email?? '';
+password = this.form.value.password?? '';
+
 onSubmit() {
 this.submitted = true;
 if(this.form.invalid){
   return;
 }
-console.log(this.form.value);
-this.form.reset();
-this.submitted = false;
+this.auth.login(this.email, this.password);
+this.router.navigate(['/'])
 }}
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+auth = inject(AuthService);
+router = inject(Router)
+get isLoggedIn() { return this.auth.isLoggedIn(); }
+
+logout():void{
+  this.auth.logout();
+  this.router.navigate(['/login']);
+}
 
 }
