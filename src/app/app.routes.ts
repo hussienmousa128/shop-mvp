407037580845facc82c5fb060cf64b2cfdbb09c9
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-27 23:30:53 +0300

    feat: add routes and enable routerLink in header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Products } from './features/products/products';
+import { Cart } from './features/cart/cart';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:Home},
+  {path: 'products', component:Products},
+  {path: 'cart', component: Cart },
+  {path: 'login', component: Login },
+  {path: '**', redirectTo: '' },
+];
