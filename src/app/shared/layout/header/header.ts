f2c876142af3faf13b9d013c1ce3ff1d0bacc000
hussienmousa128 <hussien.mousa128@gmail.com>
2025-12-28 22:14:41 +0300

    refactor(layout): move header to shared layout

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../../core/auth/auth.service';
+import { AuthService } from '../../../core/auth/auth.service';
 import { CommonModule } from '@angular/common';
 
 
